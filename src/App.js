5a8ee9f3134c40278067cd86ee31989075effb28
dinyardin <dinyardin@gmail.com>
2021-08-04 21:55:26 -0400

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <>hey!</>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
 }
 
 export default App;
