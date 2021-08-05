1f476e7521cef44798903a7c0155f29f056d2930
dinyardin <dinyardin@gmail.com>
2021-08-05 13:20:04 -0400

    Added routing switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Services from "./components/pages/Services";
+import Design from "./components/pages/Design";
+import ContactUs from "./components/pages/ContactUs";
+import SignUp from "./components/pages/SignUp";
+import Marketing from "./components/pages/Marketing";
+import Consulting from "./components/pages/Consulting";
+import Development from "./components/pages/Development";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/design" component={Design} />
+        <Route path="/contact-us" component={ContactUs} />
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/marketing" component={Marketing} />
+        <Route path="/consulting" component={Consulting} />
+        <Route path="/development" component={Development} />
+      </Switch>
     </BrowserRouter>
   );
 }
